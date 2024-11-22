e8e117ac66b120b50d8f44a983ba27c1843a7c67
Nuno Fernandes <nuno.o.fernandes@outlook.pt>
2024-11-22 14:16:23 +0000

    Game unit tests

--- src/main/java/org/grupogjl/Game.java
+++ src/main/java/org/grupogjl/Game.java
@@ -15,27 +15,33 @@ public class Game {
     private static Game instance;
     private final LanternaGui gui;
     private State state = new StateMenu();
+
     private Game() throws FontFormatException, IOException, URISyntaxException {
         this.gui = new LanternaGui(416, 207);
         WavAudioPlayer.playSound("gameSound.wav");
-
     }
+
     public Game(LanternaGui gui) { // for testing
         this.gui = gui;
 
     }
+
     public static synchronized Game getInstance() throws FontFormatException, IOException, URISyntaxException {
         if (instance == null) {
             instance = new Game();
         }
         return instance;
     }
+
     public State getState() {
         return state;
     }
+
     public LanternaGui getGui() {
         return gui;
     }
+
+
     public void run() throws InterruptedException, IOException {
         int FPS = 60;
         long targetFrameTime = 1000 / FPS;
@@ -48,29 +54,36 @@ public class Game {
                 Thread.sleep(sleepTime);
             }
         }
-        System.exit(0);
     }
+
     public void setStateMenu() {
         state = new StateMenu();
     }
+
     public void setStateGame() throws IOException {
         state = new StateGame();
     }
+
     public void setStatePause() {
         state = new StatePause((StateGame) state);
     }
+
     public void setStateNull() {
         this.state = null;
     }
+
     public StateGame getStateGame() { //testing
         return (StateGame) state;
     }
+
     public void setStateGame(State parent) {
         state = parent;
     }
+
     public StateMenu getStateMenu() {
         return (StateMenu) state;
     }
+
     public StatePause getStatePause() {
         return (StatePause) state;
     }
